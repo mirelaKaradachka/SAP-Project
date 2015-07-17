ca4db893bfdeef53399017af2d549edd76cbceba
Sasho94 <alexandermladjov@abv.bg>
2015-07-17 13:34:26 +0300

    changes calculator

--- Fitness/src/controller/FitnessCalculator.java
+++ Fitness/src/controller/FitnessCalculator.java
@@ -13,7 +13,8 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/FitnessCalculator")
 public class FitnessCalculator extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+        
+	
     /**
      * @see HttpServlet#HttpServlet()
      */
